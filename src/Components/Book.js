f572932451170113ee9b2fbf247635dde6c55018
Albert Jennifer <horvathalbertjennifer@gmail.com>
2018-08-12 12:16:26 +0200

    componentDidMount update

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -4,3 +4,9 @@ class Book extends React.Component {
   state = {
     shelf: 'none'
   }
+ componentDidMount() {
+    const { book } = this.props;
+    if(book.bookcase) {
+      this.setState({ shelf: book.shelf })
+    }
+  }
