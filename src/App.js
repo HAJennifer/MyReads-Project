0a8b268753036e9bb5269e450a228f03ecf144f0
Albert Jennifer <horvathalbertjennifer@gmail.com>
2018-08-12 12:11:42 +0200

    text input

--- src/App.js
+++ src/App.js
@@ -72,8 +72,8 @@ componentDidMount() {
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link> 
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-
+                 <input type="text" placeholder="Search by title or author" value={query} onChange={(event) => 
+		this.updateQuery(event.target.value)} /> 
               </div>
             </div>
             <div className="search-books-results">
