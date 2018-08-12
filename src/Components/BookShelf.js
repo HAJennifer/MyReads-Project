1682aae1e61ac6aff0081d9e100a602698014008
Albert Jennifer <horvathalbertjennifer@gmail.com>
2018-08-12 12:19:48 +0200

    Update BookShelf.js

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -3,3 +3,8 @@ import {Link} from 'react-router-dom'
 import Book from './Book'
 import NotFound from './NotFound'
 import PropTypes from 'prop-types'
+
+class BookShelf extends React.Component {
+  updateBook = (book, shelf) => {
+    this.props.onChangeShelf(book, shelf) 
+  }
